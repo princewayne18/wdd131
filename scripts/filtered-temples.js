e343346cc63d4c1876f482bca8690c2eac1a841d
princewayne18 <pdube1@byupathway.edu>
2026-02-08 03:00:27 +0100

    removing comments

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -70,7 +70,6 @@ const temples = [
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
-    // Add more temple objects here...
   {
         templeName: "Harare Zimbabwe",
         location: "Harare, Zimbabwe",
